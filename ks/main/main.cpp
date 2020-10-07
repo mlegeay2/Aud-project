31c4d677930920b7713da308fe887c5d9d79fa59
Agathe <a.bernetrollande@gmail.com>
2020-10-07 12:06:31 +0200

    test_bouton

--- ks/main/main.cpp
+++ ks/main/main.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <iostream>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "../../lib/ES8388.h"
-
+#include "../../lib/ButtonHandler.h"
 #include "AudioDsp.h"
 
 extern "C" {
@@ -21,18 +22,27 @@ void app_main(void)
   AudioDsp audioDsp(48000,16);
   audioDsp.start();
 
+  ButtonHandler buttonHandler(4,20);
+
   // infinite loop playing a little melody
-  int melody[16] = {72, 72, 72, 72, 81, 81, 81, 81, 76, 76, 76, 76, 79, 79, 79, 79};
-  int tonalites[16] = {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0};
+  int melody[16] = {72, 76, 72, 76, 72, 81, 76, 72, 81, 76, 72, 81, 76, 72, 81, 76};
+  int tonalites[16] = {0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1};
   //int gain_premiere_corde = {};
 
   // infinite loop
   while(1) {
+    buttonHandler.tick();
+    
+    float pulse = buttonHandler.getValue()*100 + 80;
+    std::cout << "Carrier Freq: " << pulse << "\n";
+    
+    
     for(int i=0; i< 16 ; i++){  
 
       audioDsp.setFreq(melody [i], tonalites[i]);
       audioDsp.trigger();
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      //Reglage du tempo
+      vTaskDelay(pulse / portTICK_PERIOD_MS);
     }
   }
 
